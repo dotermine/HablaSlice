d974171d00316eed8ddd922dd627eff7f2070a7d
dotermine <ilyag791@gmail.com>
2026-07-08 00:27:41 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,40 @@
+// sw.js
+const CACHE_NAME = 'hablaslice-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+  // Добавьте сюда все CSS, JS и другие ресурсы, которые использует ваше приложение
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => new Response('Offline', { status: 503 }))
+  );
+});
